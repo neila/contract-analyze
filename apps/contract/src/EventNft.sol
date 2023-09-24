5ed76f6adc0391c30dd0022df2d62006f6dc3f9f
neila <neila@shiftbase.xyz>
2023-09-25 06:55:06 +0900

    boilerplate 165

--- apps/contract/src/EventNft.sol
+++ apps/contract/src/EventNft.sol
@@ -4,9 +4,7 @@ pragma solidity ^0.8.19;
 import "../lib/openzeppelin-contracts/contracts/utils/introspection/ERC165.sol";
 
 contract EventNft is ERC165 {
-    // function supportsInterface(bytes4 interfaceID) external view  returns (
-    //     interfaceId == type(IERC1155).interfaceId ||
-    //     interfaceId == type(IERC721Metadata).interfaceId ||
-    //     super.supportsInterface(interfaceId);
-    // );
+    function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
+        return interfaceId == type(IERC165).interfaceId;
+    }
 }
